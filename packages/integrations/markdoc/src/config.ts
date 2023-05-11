6f5ea9c2f9c347792d482df57e92341ac3d66766
Carol Delwing <caroldelwing@hotmail.com>
2023-05-11 13:07:51 -0400

    update and reorganize folders and files

--- packages/integrations/markdoc/src/config.ts
+++
@@ -1,6 +0,0 @@
-import type { ConfigType as MarkdocConfig } from '@markdoc/markdoc';
-export { default as Markdoc } from '@markdoc/markdoc';
-
-export function defineMarkdocConfig(config: MarkdocConfig): MarkdocConfig {
-	return config;
-}
--- packages/integrations/vercel/test/fixtures/image/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import vercel from '@astrojs/vercel/static';
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-	adapter: vercel({imageService: true}),
-	experimental: {
-		assets: true
-	}
-});
